407c9a102f734a7fe910a9208b70ed04c2fff558
gk <gkpyon@gmail.com>
2018-04-06 14:09:39 +0900

    Minor stuff

--- main.js
+++ main.js
@@ -3,60 +3,72 @@ const Discord = require('./lib/discord')
 const Twitter = require('./lib/twitter')
 const withHttp = require('./lib/http')
 
-const config = env('config')
+// Requires env/config.json
+const config = env('config') || {}
 
+// Requires env/discord.txt
 const discord = new Discord(env('discord'), config.storage)
 
-const twitter = new Twitter(env('twitter'), config.screenName)
-
-const formatDate = date => date && date.toUTCString().slice(5)
-
-const statusReport = () =>
-  `status:
-started: ${formatDate(discord.startedAt)}
-ping: ${discord.client.ping}
-pid: ${process.pid}`
-
-const serverReport = guild =>
-  `server:
-created at: ${formatDate(guild.createdAt)}`
-
+// Sending a message to a channel on first ready event
 discord.pipe(
   'ready',
   {server: 'Test Server', channel: 'bot'}
 )
 
+// Sending multiple messages to a channel on first ready event
 discord.pipe(
   [1, 2, 3],
   {server: 'Test Server', channel: 'bot'}
 )
 
-discord.pipe(
-  twitter.avatar.bind(twitter),
-  {server: 'Test Server', channel: 'bot'}
-)
+if (config.screenName) {
+  // Requires env/twitter.json, watching config.screenName account
+  const twitter = new Twitter(env('twitter'), config.screenName)
 
-discord.pipe(
-  onTweet => twitter.tweet(tweet => {
-    console.log(tweet)
-    onTweet({text: `...`})
-  }),
-  {server: 'Test Server', channel: 'bot'}
-)
+  // Sending twitter avatars to a channel
+  discord.pipe(
+    twitter.avatar.bind(twitter),
+    {server: 'Test Server', channel: 'bot'}
+  )
 
+  // Sending tweets to a channel
+  discord.pipe(
+    onTweet => twitter.tweet(tweet =>
+      onTweet({text: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`})
+    ),
+    {server: 'Test Server', channel: 'bot'}
+    // ^ use offline to only send when a user is offline
+  )
+}
+
+// Bot status report, for status command and HTTP
+const statusReport = () =>
+  `status:
+started: ${discord.startedAt.toUTCString().slice(5)}
+ping: ${Math.round(discord.client.ping)}
+pid: ${process.pid}`
+
+// Server info for server command
+const serverReport = guild =>
+  `server:
+created at: ${guild.createdAt.toUTCString().slice(5)}`
+
+// Regular commands
 discord.commands({
-  [/~server/]: message => message.reply(serverReport(message.guild)),
   [/~status/]: message => message.reply(statusReport()),
+  [/~server/]: message => message.reply(serverReport(message.guild)),
   [/~set\s+(.+?)\s*=\s*(.+)/]: (_, k, v) => discord.set(k, v),
   [/~get\s+(.+)/]: (message, k) => discord.get(k) && message.channel.send(discord.get(k))
 })
 
+// Commands only available to config.admins
 discord.commands(config.admins, {
   [/~clear\s+(.+)/]: (message, id) =>
     message.channel.fetchMessages({after: id}).then(messages => {
-      message.channel.bulkDelete(Array.from(messages.values()))
-    }),
+      message.channel.bulkDelete(Array.from(messages.values())).catch(console.log)
+    }).catch(console.log),
   [/~stop\s+(.+)/]: (_, pid) => pid === process.pid.toString() && process.exit()
 })
 
+// Start HTTP server and the bot
 withHttp(config.port || env('port') || 80, statusReport, discord.start.bind(discord))
--- readme.md
+++ readme.md
@@ -10,6 +10,18 @@ Basic [discord](https://discordapp.com/developers) bot that can be deployed on [
 
 ## Usage
 * Create `env/discord.txt` (discord bot token), `env/twitter.json` ([twit](https://github.com/ttezel/twit) options, for twitter features), etc.
+
+      mkdir -p env
+      echo 'your discord bot token' > env/discord.txt
+      # for twitter:
+      # create env/twitter.json
+      # add screenName in env/config.json
+
 * `sh now local` : deploy locally (use `sudo` or `port` environment variable if needed, do `npm i` first to install dependencies).
+
+      sh now local # or
+      port=3000 sh now local # or
+      sudo sh now local
+
 * `sh now stop` : stop all running [now](https://zeit.co/) instances.
 * `sh now deploy` : deploy on [now](https://zeit.co/).
